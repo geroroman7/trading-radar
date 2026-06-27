ff7497ada4188c2f257c12b8414b0ea4ea848921
geroroman7 <r.fell@gmx.net>
2026-06-27 09:31:33 +0200

    Fix subheader typo in app.py

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 
-st.subheader("FRG Analyse eintigs chek)
 # 1. App-Styling für das Smartphone (FRG Invest Layout)
 st.set_page_config(page_title="FRG Invest - Trading Control", page_icon="📈", layout="wide")
 st.title("📈 FRG Invest")
-st.subheader("FRG Enstigs Chek")
+st.subheader("FRG Einstiegs Check")
 
 # 2. Eingabe des Tickers (Wandelt Eingabe automatisch in Großbuchstaben um)
 ticker_input = st.text_input("Asset-Kürzel eingeben (z.B. GOOG, BMW.DE, MP):", "GOOG").strip().upper()
